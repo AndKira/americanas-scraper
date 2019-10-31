67046905fb2860c30c62d263bbf157971eff67c7
AndreBarciela <barciela@strsistemas.com.br>
2019-10-31 20:16:30 -0300

    Todos as variáveis necessárias adquiridas.

--- americanas_scraper/spiders/Americanas.py
+++ americanas_scraper/spiders/Americanas.py
@@ -5,7 +5,7 @@ import json
 class AmericanasSpider(scrapy.Spider):
     name = 'Americanas'
     allowed_domains = ['americanas.com.br/']
-    start_urls = ['http://americanas.com.br/produto/134186808']
+    start_urls = ['http://americanas.com.br/produto/423474401']
 
     def parse(self, response):
         html_body = response.css('div#content script::text').getall()
@@ -13,8 +13,13 @@ class AmericanasSpider(scrapy.Spider):
         jsonresponse = json.loads(html_body[0])
 
         code   = jsonresponse["@graph"][2]["url"].replace('https://www.americanas.com.br/produto/', '')
+        breadcrumb = []
+        for position in jsonresponse["@graph"][3]["itemListElement"]:
+            breadcrumb.append(position["item"]["name"])
         name   = jsonresponse["@graph"][4]["name"]
         img    = jsonresponse["@graph"][4]["image"]["url"]
-        seller = jsonresponse["@graph"]["name"]
+        seller = jsonresponse["@graph"][0]["name"]
+        price  = jsonresponse["@graph"][4]["offers"]["price"]
 
-        yield {"id": code, "name": name, "img": img, "seller": seller, "json": jsonresponse["@graph"][4]}
+
+        yield {"id": code, "breadcrumb": breadcrumb, "name": name, "img": img, "seller": seller, "price": price}
